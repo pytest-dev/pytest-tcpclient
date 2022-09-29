6373c6f3950edac91f3809ee4399140ba6124b10
Anders Lindstrom <anders@anderslindstrom.com>
2022-09-29 17:26:51 +1000

    Renamed test case

--- tests/test_mocktcp.py
+++ tests/test_mocktcp.py
@@ -6,9 +6,8 @@ import asyncio
 
 
 @pytest.mark.asyncio()
-async def test(tcpserver):
+async def test_second_connection_causes_failure(tcpserver):
     assert isinstance(tcpserver, MockTcpServer)
     await asyncio.open_connection(None, tcpserver.service_port)
-
     with pytest.raises(Exception, match="Client is already connected"):
         await asyncio.open_connection(None, tcpserver.service_port)
